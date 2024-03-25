bd16316d1b0d7e4c6c5252155db08cbba5c7fb9a
Justin Rose <89818832+monk-iatsu@users.noreply.github.com>
2024-03-24 19:47:07 -0500

    update

--- generator/import_new_csv.py
+++ generator/import_new_csv.py
@@ -27,7 +27,6 @@ try:
 except json.JSONDecodeError:
     with open(id_file, "w") as f:
         json.dump(fp=f, obj={"id_list": [], "id_size": id_size}, indent=True)
-<<<<<<< Updated upstream
         id_data = {"id_list": {}, "id_size": id_size}
 
 
@@ -56,82 +55,10 @@ def get_id() -> str:
     id_data["id_list"].append(_id)
     with open(id_file, "w") as f:
         json.dump(id_data, f)
-=======
-        id_data = {"id_list": [], "id_size": id_size}
-
-
-def read_csv_in(file: str) -> List[Dict[str, str]]:
-    """
-    Reads a CSV file and returns a list of dictionaries representing the data.
-
-    Parameters:
-    - file (str): The path to the CSV file.
-
-    Returns:
-    - List[Dict[str, str]]: A list of dictionaries, where each dictionary represents a row in the CSV file. The keys of the dictionaries are the column names in the CSV file, and the values are the corresponding values in each row.
-
-    Example:
-    data = read_csv_in("data.csv")
-    for row in data:
-        print(row["creation_id"], row["filename"], row["created_at"], row["prog_id"])
-"""
-    output = []
-    with open(file) as f:
-        for line in csv.DictReader(f):
-            data = {
-                "creation_id": line["creation_id"],
-                "filename": line["filename"],
-                "created_at": line["created_at"],
-                "prog_id": get_id()
-            }
-            output.append(data)
-    yield from output
-
-
-def move_img(file_in: str, file_out: str) -> None:
-    os.rename(file_in, file_out)
-
-
-def store_id(id: str) -> None:
-    id_data["id_list"].append(id)
-
-def get_id() -> str:
-    """
-    Generate a unique ID.
-
-    Returns:
-        str: A unique ID.
-
-"""
-    global id_size, id_data
-    _id = secrets.token_hex(id_size)
-    while _id in id_data["id_list"]:
-        _id = secrets.token_hex(id_size)
-    id_data["id_list"].add(_id)
-    store_id(_id)
->>>>>>> Stashed changes
     return _id
 
 
 def existance_test(creation_id: str) -> bool:
-<<<<<<< Updated upstream
-=======
-    """
-    Reads a CSV file and returns a list of dictionaries representing the data.
-
-    Parameters:
-    - file (str): The path to the CSV file.
-
-    Returns:
-    - List[Dict[str, str]]: A list of dictionaries, where each dictionary represents a row in the CSV file. The keys of the dictionaries are the column names in the CSV file, and the values are the corresponding values in each row.
-
-    Example:
-    data = read_csv_in("data.csv")
-    for row in data:
-        print(row["creation_id"], row["filename"], row["created_at"], row["prog_id"])
-
-"""
->>>>>>> Stashed changes
     with open(data_file) as f:
         for line in csv.DictReader(f):
             if creation_id == line["creation_id"]:
@@ -139,7 +66,6 @@ def existance_test(creation_id: str) -> bool:
         return False
 
 
-<<<<<<< Updated upstream
 def update_data() -> None:
     with open(data_file) as f:
         data = list(csv.DictReader(f))
@@ -158,9 +84,3 @@ def update_data() -> None:
 
 if __name__ == "__main__":
     update_data()
-=======
-atexit.register
-def exit_handler():
-    with open(id_file, "w") as f:
-        json.dump(fp=f, obj=id_data)
->>>>>>> Stashed changes
